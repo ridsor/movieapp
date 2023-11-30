c8abf7361b7d25f332df99587180a1a5356a804c
ridsor <ridsorgamerz@gmail.com>
2023-11-30 14:25:25 +0700

    memperbaiki date review detail movie

--- src/components/fragments/MovieDetail/MovieReviews.tsx
+++ src/components/fragments/MovieDetail/MovieReviews.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useOutletContext } from "react-router-dom";
 import MovieNotFound from "../Movie/MovieNotFound";
 import MovieReviewItem from "./MovieReviewItem";
@@ -16,6 +17,19 @@ interface Review {
 const MovieReviews = () => {
   const { reviews } = useOutletContext<{ reviews: Review[] }>();
 
+  const dateConvert = useCallback((date: string) => {
+    const example = new Date(date);
+    const result = new Intl.DateTimeFormat("id", {
+      weekday: "long",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(example);
+    return result;
+  }, []);
+
   return (
     <div className="px-2 mb-8">
       <h2 className="text-3xl font-bold relative after:content-[''] after:block after:w-full after:absolute after:border-b-2 after:top-1/2 after:-translate-y-1/2 after:border-[#cdcdcd] after:right-0 mb-8 [text-shadow:0px_2px_2px_rgba(0,0,0,0.25)] after:-z-10">
@@ -24,7 +38,11 @@ const MovieReviews = () => {
       <div className="reviews flex gap-x-5 gap-y-8 flex-col">
         {reviews.length > 0 ? (
           reviews.map((review) => (
-            <MovieReviewItem review={review} key={review.id} />
+            <MovieReviewItem
+              review={review}
+              key={review.id}
+              dateConvert={dateConvert}
+            />
           ))
         ) : (
           <MovieNotFound />
